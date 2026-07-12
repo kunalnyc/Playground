83a16f52c59d0c21d8df4e0940b905d8113b3551
Kunal <kunalkmr9717@outlook.com>
2026-07-12 20:00:35 +0530

    fix tpye for typescript

--- build/js/main.js
+++ build/js/main.js
@@ -3,7 +3,10 @@
 let username = 'kunal kumar';
 console.log(username);
 let a = 12;
-let b = '6';
+let b = 6;
 let c = 2;
+console.log(a / b);
+console.log(a + b + c);
+console.log(c * b);
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@
 let username =  'kunal kumar'
 console.log(username)
 
-let a = 12
-let b = '6'
-let c = 2
+let a: number = 12
+let b: number = 6
+let c: number = 2
+
+console.log(a/b)
+console.log(a+b+c)
+console.log(c*b)
