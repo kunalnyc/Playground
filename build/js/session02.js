a12f6fb32ff4ecca16d1c3a90552e109dd5998ee
Kunal <kunalkmr9717@outlook.com>
2026-07-13 20:53:48 +0530

    Array

---
+++ build/js/session02.js
@@ -0,0 +1,10 @@
+let stringUsernames = ['olivia', 'emma', 'emily', 'sophia'];
+let Ids = [1, 2, 'olivia', 4, 'emma'];
+let mixedData = ['olivia', 1, 'emma', 2, 'emily', 3, 'sophia', 4, true];
+stringUsernames[0] = 'Ancilia';
+stringUsernames.push('Kunal');
+Ids[0] = 19;
+Ids.unshift('Rockstar');
+Ids = stringUsernames;
+export {};
+//# sourceMappingURL=session02.js.map
---
+++ src/session02.ts
@@ -0,0 +1,12 @@
+
+let stringUsernames = ['olivia', 'emma', 'emily', 'sophia']
+let Ids = [1, 2, 'olivia', 4, 'emma']
+let mixedData = ['olivia', 1, 'emma', 2, 'emily', 3, 'sophia', 4, true]
+
+stringUsernames[0] = 'Ancilia'
+stringUsernames.push('Kunal')
+
+Ids[0] = 19;
+Ids.unshift('Rockstar');
+
+Ids = stringUsernames;
